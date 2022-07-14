9bbd28e3788a76ac6b514c28d45dff4cf18e11ba
joehenrickson <joehenrickson@gmail.com>
2022-07-13 20:23:31 -0500

    5 day forecast added

--- script.js
+++ script.js
@@ -74,50 +74,119 @@ $("#current").html(newDiv);
 
 var latitude = response.coord.latitude;
 var longitude = response.coord.longitude;
-var uvIndex = "https://api.openweathermap.org/data/2.5/uvi?&appid=ecc0be5fd92206da3aa90cc41c13ca56&lat=" + lat  + "&lon=" + lon;
+var uvIndex =
+  "https://api.openweathermap.org/data/2.5/uvi?&appid=ecc0be5fd92206da3aa90cc41c13ca56&lat=" +
+  lat +
+  "&lon=" +
+  lon;
 
-        $.ajax({
-            url: uvIndex,
-            method: 'GET'
-        }).then(function (response) {
-            $('#uvIndex-display').empty();
-            var uvIndexResults = response.value;
-            //create HTML for new div
-            var uvlIndex = $("<button class='btn bg-success'>").text("UV Index: " + response.value);
-      
-            $('#uvl-display').html(uvlIndex);
-    
-        });
-    });
+$.ajax({
+  url: uvIndex,
+  method: "GET",
+}).then(function (response) {
+  $("#uvIndex-display").empty();
+  var uvIndexResults = response.value;
+  //create HTML for new div
+  var uvlIndex = $("<button class='btn bg-success'>").text(
+    "UV Index: " + response.value
+  );
 
-    $("#select-city").on("click", function (event) {
-        // Preventing the button from trying to submit the form......
-        event.preventDefault();
-        // Storing the city name........
-        var cityInput = $("#city-input").val().trim();
-    
-        //save search term to local storage.....
-        var textContent = $(this).siblings("input").val();
-        var storearr = [];
-        storearr.push(textContent);
-        localStorage.setItem('cityName', JSON.stringify(storearr));
-      
-        searchCity(cityInput);
-        pageLoad();
-    });
+  $("#uvIndex-display").html(uvlIndex);
+});
+//-- 5 day forecast function --//
 
-    function pageLoad () {
-        var lastSearch = JSON.parse(localStorage.getItem("cityName"));
-        var searchDiv = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>").text(lastSearch);
-        var psearch = $("<div>");
-        psearch.append(searchDiv)
-        $("#searchhistory").prepend(psearch);
+$.ajax({
+  url: forcast,
+  method: "GET",
+}).then(function (response) {
+  var results = response.list;
+  $("#5day").empty();
+  for (var i = 0; i < results.length; i += 8) {
+    var fiveDayDiv = $(
+      "<div class='card shadow-lg text-white bg-primary mx-auto mb-10 p-2' style='width: 8.5rem; height: 11rem;'>"
+    );
+    var date = results[i].dt_txt;
+    var setDate = date.substr(0, 10);
+    var temp2 = results[i].main.temp;
+    var hum2 = results[i].main.humidity;
+    var h5date = $("<h5 class='card-title'>").text(setDate);
+    var pTemp = $("<p class='card-text'>").text("Temp: " + temp2);
+    var pHum = $("<p class='card-text'>").text("Humidity " + hum2);
+
+    var weather = results[i].weather[0].main;
+
+    if (weather === "Rain") {
+      var icon = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/09d.png"
+      );
+      icon.attr("style", "height: 40px; width: 40px");
+    } else if (weather === "Clouds") {
+      var icon = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/03d.png"
+      );
+      icon.attr("style", "height: 40px; width: 40px");
+    } else if (weather === "Clear") {
+      var icon = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/01d.png"
+      );
+      icon.attr("style", "height: 40px; width: 40px");
+    } else if (weather === "Drizzle") {
+      var icon = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/10d.png"
+      );
+      icon.attr("style", "height: 40px; width: 40px");
+    } else if (weather === "Snow") {
+      var icon = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/13d.png"
+      );
+      icon.attr("style", "height: 40px; width: 40px");
     }
-    
-    //Event deligation...
-    $("#searchhistory").on('click', '.btn', function(event) {
-    event.preventDefault();
-        console.log($(this).text());
-        searchCity($(this).text());
-    
-    });
+
+    //append items to.......
+    fiveDayDiv.append(h5date);
+    fiveDayDiv.append(icon);
+    fiveDayDiv.append(pTemp);
+    fiveDayDiv.append(pHum);
+    $("#5day").append(fiveDayDiv);
+  }
+});
+
+pageLoad();
+
+$("#select-city").on("click", function (event) {
+  // Preventing the button from trying to submit the form......
+  event.preventDefault();
+  // Storing the city name........
+  var cityInput = $("#city-input").val().trim();
+
+  //save search term to local storage.....
+  var textContent = $(this).siblings("input").val();
+  var storearr = [];
+  storearr.push(textContent);
+  localStorage.setItem("cityName", JSON.stringify(storearr));
+
+  searchCity(cityInput);
+  pageLoad();
+});
+
+function pageLoad() {
+  var lastSearch = JSON.parse(localStorage.getItem("cityName"));
+  var searchDiv = $(
+    "<button class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>"
+  ).text(lastSearch);
+  var psearch = $("<div>");
+  psearch.append(searchDiv);
+  $("#searchhistory").prepend(psearch);
+}
+
+//Event deligation...
+$("#searchhistory").on("click", ".btn", function (event) {
+  event.preventDefault();
+  console.log($(this).text());
+  searchCity($(this).text());
+});
